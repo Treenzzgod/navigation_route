130eca6f28ced9d8805f09e64f18699a4be29077
Treenzzgod <gio12619@gmail.com>
2022-03-23 10:53:29 +0700

    page detail

--- lib/models/Item.dart
+++ lib/models/Item.dart
@@ -2,10 +2,18 @@
 // ignore: file_names
 // ignore_for_file: file_names
 
+import 'package:flutter/cupertino.dart';
+
 class Item {
   final String name;
-  final int? weight;
-  final int? price;
+  final int weight;
+  final int price;
+  final String image;
 
-  Item({required this.name, required this.price, required this.weight});
+  Item({
+    required this.name,
+    required this.price,
+    required this.weight,
+    required this.image,
+  });
 }
--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -12,14 +12,107 @@ class ItemPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Shopping list'),
       ),
-      body: Center(
-        child: Text(item.name +
-            ' with ' +
-            item.price.toString() +
-            ' and weight ' +
-            item.weight.toString() +
-            ' kg '),
+      body: pageview(item: item),
+    );
+  }
+}
+
+class pageview extends StatelessWidget {
+  const pageview({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
+
+  final Item item;
+
+  @override
+  Widget build(BuildContext context) {
+    final double headerTop = MediaQuery.of(context).size.height * 0.45;
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width: double.infinity,
+                    height: headerTop,
+                    child: Image.asset(item.image),
+                  ),
+                  Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding:
+                              EdgeInsets.only(top: 10, left: 10, right: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    item.name,
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 30),
+                                  ),
+                                  Text(
+                                    item.weight.toString() + ' KG ',
+                                    textAlign: TextAlign.end,
+                                    style: TextStyle(fontSize: 20),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                item.price.toString(),
+                                style: TextStyle(color: Colors.red[600]),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                'Produk PT.SENYUMMU MANIS',
+                                style: TextStyle(color: Colors.green[600]),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
+    //     child: Row(
+    //       Container(
+    //         child: Center(
+    //           child: Column(
+    //             children: [
+    //               Text(item.name +
+    //                   ' with ' +
+    //                   item.price.toString() +
+    //                   ' and weight ' +
+    //                   item.weight.toString() +
+    //                   ' kg '),
+    //             ],
+    //           ),
+    //         ),
+    //       ),
+    //     ),
+    // ],
   }
 }
